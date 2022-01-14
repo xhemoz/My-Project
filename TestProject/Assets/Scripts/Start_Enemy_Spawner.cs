af7f071bd3a453f8b72b1405b700ed750bf3e255
Aldo <axhemollari1@gmail.com>
2022-01-13 21:13:58 -0800

    New spawner implemented
    A random enemy will spawn at a random position in the map when the game starts.

---
+++ TestProject/Assets/Scripts/Start_Enemy_Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_Enemy_Spawner : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] enemies;
+    [SerializeField]
+    public Transform[] spawns;
+
+    private GameObject enemyRef;
+    private Transform spawnRef;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SetSpawn", 1f, .5f);
+    }
+
+    Transform GetRandomSpawnPoint(Transform[] spawnArray)
+    {
+        int index = Random.Range(0, spawnArray.Length);
+
+        return spawnArray[index];
+    }
+
+    GameObject GetRandomEnemy(GameObject[] enemies)
+    {
+        int index = Random.Range(0, enemies.Length);
+
+        return enemies[index];
+    }
+
+    void SetSpawn()
+    {
+        spawnRef = GetRandomSpawnPoint(spawns);
+        enemyRef = GetRandomEnemy(enemies);
+
+        var clone = Instantiate(enemyRef, spawnRef.position, Quaternion.identity);
+    }
+}
